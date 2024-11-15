4836bfc8c183ccfc846699c0aa8a4c6e1f9f9f34
Kohl Bromwich <9856817+kbromwich@users.noreply.github.com>
2024-11-15 21:10:44 +1300

    BugFix: Search case sensitivity not working correctly

--- src/ui/Selector/search.ts
+++ src/ui/Selector/search.ts
@@ -1,8 +1,9 @@
 
 const search = (searchText: string, options: string[], caseInsensitive: boolean) => {
+  const search = caseInsensitive ? searchText.toLowerCase() : searchText;
   return options.filter((simName) => {
     const name = caseInsensitive ? simName.toLowerCase() : simName;
-    return name.includes(searchText);
+    return name.includes(search);
   });
 };
 
